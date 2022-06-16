972243405be40b810af61dba24e240f072ce4a0c
maykel nekel <maykelnekel@gmail.com>
2022-06-16 19:56:40 -0300

    fix: atualiado yarn dev script e biblioteca utilizada

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,21 @@ import express from "express"
 import mongoose from "mongoose"
 import fotosRouter from "./routes/fotos.router"
 import loginRouter from "./routes/login.router"
+import userRouter from "./routes/user.router"
 
 require('dotenv').config()
 
 const port = 3000
 const app = express()
-const dbUrl = process.env.BD_URL_CONNECTION
+const bdUrl = process.env.BD_URL_CONNECTION
 
 app.use(express.json())
 
 app.use("/login", loginRouter)
 app.use("/fotos", fotosRouter)
+app.use("/user", userRouter)
 
-mongoose.connect(dbUrl as string)
+mongoose.connect(bdUrl as string)
 .then(()=>{
     console.log("banco conectado com sucesso")
     app.listen(port, () => console.log("app iniciado com sucesso"))})
--- src/routes/fotos.router.ts
+++ src/routes/fotos.router.ts
@@ -4,5 +4,4 @@ const fotosRouter = Router()
 
 
 
-
 export default fotosRouter
